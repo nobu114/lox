1e35e2c60e8b053b580ae0286d6f7f9ab2c4cc0d
N.Nobuoka <nao114computer@gmail.com>
2025-07-13 03:38:22 +0900

    First Commit
    環境のセットアップ

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * @file main.c
+ * @brief 
+ *
+ * Description
+ *
+ * @author Your Name
+ *
+ * @copyright Copyright (c) 2025 Your Name
+*/
+
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Hello World!!\n");
+	return 0;
+}
